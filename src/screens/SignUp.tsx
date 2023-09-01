112af99281df75fd536d9a560d68e4c14f9eaa75
Mark Avila <avilamark96@gmail.com>
2023-09-01 15:41:50 +0800

    feat: signup phone number

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import { ChangeEvent, useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -9,6 +9,7 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography, { TypographyProps } from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import MuiPhoneNumber from "mui-phone-number";
 
 function Copyright(props: TypographyProps) {
   return (
@@ -31,14 +32,64 @@ interface Props {
   handleScreen: VoidFunction;
 }
 
+type InputField =
+  | "fname"
+  | "lname"
+  | "password1"
+  | "password2"
+  | "email"
+  | "contact";
+
 export default function SignUp({ handleScreen }: Props) {
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
+  const [fname, setFname] = useState<string>("");
+  const [lname, setLname] = useState<string>("");
+  const [password1, setPassword1] = useState<string>("");
+  const [password2, setPassword2] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [contact, setContact] = useState<string>("");
+  // const [bday, setContact] = useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
     console.log({
       email: data.get("email"),
       password: data.get("password"),
     });
+  };
+
+  const handleOnChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    target: InputField
+  ) => {
+    switch (target) {
+      case "fname":
+        setFname(e.currentTarget.value);
+        break;
+      case "lname":
+        setLname(e.currentTarget.value);
+        break;
+      case "password1":
+        setPassword1(e.currentTarget.value);
+        break;
+      case "password2":
+        setPassword2(e.currentTarget.value);
+        break;
+      case "email":
+        setEmail(e.currentTarget.value);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  // const handleContactOnChange = (value: string) => setContact(value);
+
+  const handleContactOnChange: (
+    e: string | ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void = (e) => {
+    setContact(e as string);
   };
 
   return (
@@ -68,8 +119,12 @@ export default function SignUp({ handleScreen }: Props) {
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
+                  size="small"
                   autoComplete="given-name"
                   name="firstName"
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    handleOnChange(e, "fname")
+                  }
                   required
                   fullWidth
                   id="firstName"
@@ -79,8 +134,12 @@ export default function SignUp({ handleScreen }: Props) {
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField
+                  size="small"
                   required
                   fullWidth
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    handleOnChange(e, "lname")
+                  }
                   id="lastName"
                   label="Last Name"
                   name="lastName"
@@ -89,20 +148,53 @@ export default function SignUp({ handleScreen }: Props) {
               </Grid>
               <Grid item xs={12}>
                 <TextField
+                  size="small"
                   required
                   fullWidth
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    handleOnChange(e, "email")
+                  }
                   id="email"
                   label="Email Address"
                   name="email"
+                  type="email"
                   autoComplete="email"
                 />
               </Grid>
               <Grid item xs={12}>
+                <MuiPhoneNumber
+                  variant="outlined"
+                  defaultCountry={"ph"}
+                  size="small"
+                  fullWidth
+                  onChange={handleContactOnChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
                 <TextField
+                  size="small"
                   required
                   fullWidth
                   name="password"
                   label="Password"
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    handleOnChange(e, "password1")
+                  }
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  size="small"
+                  required
+                  fullWidth
+                  name="password-2"
+                  label="Re-enter password"
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    handleOnChange(e, "password2")
+                  }
                   type="password"
                   id="password"
                   autoComplete="new-password"
