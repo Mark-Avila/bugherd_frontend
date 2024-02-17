ae6981c3793b8e675955b7dc509cc062960402a2
Mark Avila <avilamark96@gmail.com>
2024-02-17 23:00:08 +0800

    feat: improved ticket skeletons

--- src/components/stateless/TicketList.tsx
+++ src/components/stateless/TicketList.tsx
@@ -1,4 +1,12 @@
-import { Box, List, Paper, Skeleton, Typography } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  Paper,
+  Skeleton,
+  Stack,
+  Typography,
+} from "@mui/material";
 import TicketListItem from "./TicketListItem";
 import { TicketWithUser } from "../../types";
 import dayjs from "dayjs";
@@ -10,32 +18,45 @@ interface Props {
 function TicketList({ tickets }: Props) {
   return (
     <Paper variant="outlined">
-      {tickets
-        ? tickets.length !== 0 && (
-            <List disablePadding>
-              {tickets.map((ticket) => (
-                <TicketListItem
-                  key={ticket.id}
-                  id={ticket.id}
-                  title={ticket.title}
-                  number={(ticket.num as number).toString()}
-                  created={dayjs(ticket.created_at).format("LL")}
-                  author={`${ticket.fname} ${ticket.lname}`}
-                  priority={ticket.priority}
-                  status={ticket.status}
+      {tickets ? (
+        tickets.length !== 0 && (
+          <List disablePadding>
+            {tickets.map((ticket) => (
+              <TicketListItem
+                key={ticket.id}
+                id={ticket.id}
+                title={ticket.title}
+                number={(ticket.num as number).toString()}
+                created={dayjs(ticket.created_at).format("LL")}
+                author={`${ticket.fname} ${ticket.lname}`}
+                priority={ticket.priority}
+                status={ticket.status}
+              />
+            ))}
+          </List>
+        )
+      ) : (
+        <List>
+          {[1, 2, 3, 4, 5].map((item, index) => (
+            <ListItem divider>
+              <Stack>
+                <Skeleton
+                  key={item * index}
+                  variant="text"
+                  width={120}
+                  height={24}
                 />
-              ))}
-            </List>
-          )
-        : [1, 2, 3, 4, 5].map((item, index) => (
-            <Skeleton
-              key={item * index}
-              variant="rounded"
-              width="100%"
-              height={48}
-              sx={{ marginTop: 1 }}
-            />
+                <Skeleton
+                  key={item * index}
+                  variant="text"
+                  width={300}
+                  height={24}
+                />
+              </Stack>
+            </ListItem>
           ))}
+        </List>
+      )}
       {tickets && tickets.length === 0 && (
         <Box sx={{ p: 4, height: 200 }}>
           <Typography variant="h5" color="InactiveCaptionText">
