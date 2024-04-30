bca489bbcb247888a23163660313cdab76664d81
Mark Avila <avilamark96@gmail.com>
2024-04-30 13:57:19 +0800

    fix: removed useBreadcrumbs from hooks barrel

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,6 +2,5 @@ import useSnackError from "./useSnackError";
 import useToggle from "./useToggle";
 import useSet from "./useSet";
 import useDebounce from "./useDebounce";
-import useBreadcrumbs from "./useBreadcrumbs";
 
-export { useSnackError, useDebounce, useToggle, useSet, useBreadcrumbs };
+export { useSnackError, useDebounce, useToggle, useSet };
