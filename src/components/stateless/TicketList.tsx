290d7300db0e7819ae414612a03c5e64471bf991
Mark Avila <avilamark96@gmail.com>
2024-06-29 16:07:39 +0800

    feat: added pagination to profile tickets

--- src/components/stateless/TicketList.tsx
+++ src/components/stateless/TicketList.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   List,
   ListItem,
+  Pagination,
   Paper,
   Skeleton,
   Stack,
@@ -22,47 +23,49 @@ function TicketList({ tickets }: Props) {
   const theme = useTheme();
   const { mode } = useContext(ColorModeContext);
   return (
-    <Paper
-      variant="outlined"
-      sx={{ backgroundColor: theme.palette.background.paper }}
-    >
-      {tickets ? (
-        tickets.length !== 0 && (
-          <List disablePadding>
-            {tickets.map((ticket, index) => (
-              <TicketListItem
-                key={index}
-                id={ticket.id}
-                title={ticket.title}
-                number={(ticket.num as number).toString()}
-                created={dayjs(ticket.created_at).format("LL")}
-                author={`${ticket.fname} ${ticket.lname}`}
-                priority={ticket.priority}
-                status={ticket.status}
-              />
+    <Stack>
+      <Paper
+        variant="outlined"
+        sx={{ backgroundColor: theme.palette.background.paper }}
+      >
+        {tickets ? (
+          tickets.length !== 0 && (
+            <List disablePadding>
+              {tickets.map((ticket, index) => (
+                <TicketListItem
+                  key={index}
+                  id={ticket.id}
+                  title={ticket.title}
+                  number={(ticket.num as number).toString()}
+                  created={dayjs(ticket.created_at).format("LL")}
+                  author={`${ticket.fname} ${ticket.lname}`}
+                  priority={ticket.priority}
+                  status={ticket.status}
+                />
+              ))}
+            </List>
+          )
+        ) : (
+          <List>
+            {[1, 2, 3, 4, 5].map((item, index) => (
+              <ListItem divider key={item * index}>
+                <Stack>
+                  <Skeleton variant="text" width={120} height={24} />
+                  <Skeleton variant="text" width={300} height={24} />
+                </Stack>
+              </ListItem>
             ))}
           </List>
-        )
-      ) : (
-        <List>
-          {[1, 2, 3, 4, 5].map((item, index) => (
-            <ListItem divider key={item * index}>
-              <Stack>
-                <Skeleton variant="text" width={120} height={24} />
-                <Skeleton variant="text" width={300} height={24} />
-              </Stack>
-            </ListItem>
-          ))}
-        </List>
-      )}
-      {tickets && tickets.length === 0 && (
-        <Box sx={{ p: 4, height: 200 }}>
-          <Typography variant="h5" color="InactiveCaptionText">
-            No Tickets Yet
-          </Typography>
-        </Box>
-      )}
-    </Paper>
+        )}
+        {tickets && tickets.length === 0 && (
+          <Box sx={{ p: 4, height: 200 }}>
+            <Typography variant="h5" color="InactiveCaptionText">
+              No Tickets Yet
+            </Typography>
+          </Box>
+        )}
+      </Paper>
+    </Stack>
   );
 }
 
--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,20 +1,46 @@
-import { Grid, Stack } from "@mui/material";
+import { Box, Grid, Pagination, Stack } from "@mui/material";
 import { ProfileInfo, ProjectList, TicketList } from "../components";
 import PageSection from "../components/stateless/PageSection";
-import { useGetTicketsOfCurrentUserQuery } from "../api/ticketApiSlice";
+import { useLazyGetTicketsOfCurrentUserQuery } from "../api/ticketApiSlice";
 import { useGetCurrentProjectQuery } from "../api/projectApiSlice";
 import { ProjectWithUser } from "../types";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { setBreadcrumbs } from "../slices/breadSlice";
 
 function Profile() {
-  const tickets = useGetTicketsOfCurrentUserQuery({ limit: 5, offset: 0 });
+  const [getTickets, tickets] = useLazyGetTicketsOfCurrentUserQuery();
   const projects = useGetCurrentProjectQuery();
   const { user } = useSelector((state: RootState) => state.auth);
 
+  //Ticket Pagination
+  const [maxPage, setMaxPage] = useState(0);
+  const [currPage, setCurrPage] = useState(1);
+  const TICKET_LIMIT = 5;
+
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (user) {
+      getTickets({ limit: TICKET_LIMIT, offset: 0 })
+        .unwrap()
+        .then((res) => {
+          const pages: number = (res.count as number) / TICKET_LIMIT;
+          setMaxPage(Math.round(pages));
+        });
+    }
+  }, []);
+
+  const handlePagination = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    if (value <= maxPage && value >= 0) {
+      setCurrPage(value);
+      getTickets({ offset: (value - 1) * TICKET_LIMIT, limit: TICKET_LIMIT });
+    }
+  };
 
   useEffect(() => {
     dispatch(
@@ -50,6 +76,13 @@ function Profile() {
             </PageSection>
             <PageSection title="Tickets created">
               {<TicketList tickets={tickets.data && tickets.data.data} />}
+              <Box mt={2}>
+                <Pagination
+                  count={maxPage}
+                  onChange={handlePagination}
+                  page={currPage}
+                />
+              </Box>
             </PageSection>
           </Stack>
         </Grid>
